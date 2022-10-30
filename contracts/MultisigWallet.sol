f82fbd10e9856f235b03723a535f029710196d89
Allen Zhong <zhong.allen@gmail.com>
2022-10-31 11:16:55 +1300

    add deposit

--- contracts/MultisigWallet.sol
+++ contracts/MultisigWallet.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.9;
 
 contract MultisigWallet {
+  event Deposit(address indexed sender, uint256 amount, uint256 balance);
+
   address[] public owners;
   mapping(address => bool) public isOwner;
-  uint public numConfirmationsRequired;
+  uint256 public numConfirmationsRequired;
 
-  constructor(address[] memory _owners, uint _numConfirmationsRequired)
+  constructor(address[] memory _owners, uint256 _numConfirmationsRequired)
     public
   {
     require(_owners.length > 0, "owners required");
@@ -17,7 +19,7 @@ contract MultisigWallet {
       "invalid number of required confirmations"
     );
 
-    for (uint i = 0; i < _owners.length; i++) {
+    for (uint256 i = 0; i < _owners.length; i++) {
       address owner = _owners[i];
       require(owner != address(0), "invalid owner");
       require(!isOwner[owner], "owner not unique");
@@ -27,5 +29,20 @@ contract MultisigWallet {
     }
 
     numConfirmationsRequired = _numConfirmationsRequired;
+  }
+
+  function getOwners() public view returns (address[] memory) {
+    return owners;
+  }
+
+  // https://solidity-by-example.org/sending-ether/
+  receive() external payable {
+    emit Deposit(msg.sender, msg.value, address(this).balance);
+  }
+
+  fallback() external payable {}
+
+  function getBalance() public view returns (uint256) {
+    return address(this).balance;
   }
 }
