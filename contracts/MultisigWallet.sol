2fb5ada6187373032b1812d67101f9086e25879a
Allen Zhong <zhong.allen@gmail.com>
2022-10-30 23:02:13 +1300

    check 0x0 address

--- contracts/MultisigWallet.sol
+++ contracts/MultisigWallet.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.9;
 
 contract MultisigWallet {
+  address[] public owners;
+  // mapping(address => bool) public isOwner;
+  // uint numConfirmationsRequired;
+
   constructor(address[] memory _owners, uint256 _numConfirmationsRequired)
     public
   {
@@ -12,5 +16,10 @@ contract MultisigWallet {
         _numConfirmationsRequired <= _owners.length,
       "invalid number of required confirmations"
     );
+
+    for (uint i = 0; i < _owners.length; i++) {
+      address owner = _owners[i];
+      require(owner != address(0), "invalid owner");
+    }
   }
 }
--- test/MultisigWallet.ts
+++ test/MultisigWallet.ts
@@ -6,11 +6,15 @@ import { ethers } from "hardhat";
 describe("MultisigWallet", function () {
   async function deployFixture() {
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [owner, account1, account2] = await ethers.getSigners();
 
     const MultisigWallet = await ethers.getContractFactory("MultisigWallet");
 
-    return { MultisigWallet, owner, otherAccount };
+    const wallet = await MultisigWallet.deploy(
+      [owner.address, account1.address, account2.address],
+      3
+    );
+    return { MultisigWallet, wallet, owner, account1, account2 };
   }
 
   describe("Deployment", function () {
@@ -30,6 +34,21 @@ describe("MultisigWallet", function () {
           0
         )
       ).to.be.revertedWith("invalid number of required confirmations");
+    });
+
+    it("Should not allow add 0x0 address", async function () {
+      const [owner, otherAccount] = await ethers.getSigners();
+      const MultisigWallet = await ethers.getContractFactory("MultisigWallet");
+      await expect(
+        MultisigWallet.deploy(
+          [
+            "0x0000000000000000000000000000000000000000",
+            owner.getAddress(),
+            otherAccount.getAddress(),
+          ],
+          3
+        )
+      ).to.be.revertedWith("invalid owner");
     });
   });
 });
